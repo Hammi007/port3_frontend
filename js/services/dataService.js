e58163b69f96767205dc0637357bf0511703c830
Hammi007 <h.e.4000@hotmail.com>
2021-12-13 14:34:40 +0100

    progress with title onclick

--- js/services/dataService.js
+++ js/services/dataService.js
@@ -17,11 +17,11 @@
             .then(json => callback(json));
     };
 // Not sure if this is right?
-    /*let getTitle = (callback, id) => {
-        fetch("https://localhost:5001/api/titles/", id)
+    let getTitle = (id, callback) => {
+        fetch("https://localhost:5001/api/titles/" + id)
             .then(response => response.json())
             .then(json => callback(json));
-    };*/
+    };
 
     let removeTitle = (title) => {
         let param = {
@@ -115,7 +115,7 @@
     return {
         AddTitle,
         getTitles,
-        //getTitle,
+        getTitle,
         removeTitle,
         bookmarkTitle,
         searchTitleByGenre,
