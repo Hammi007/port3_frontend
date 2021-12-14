2febdc65ba018724f6d58457beeed95bed717482
Leon <noelem@protonmail.com>
2021-12-14 10:52:13 +0100

    changes to genre

--- js/components/genre/listGenre.js
+++ js/components/genre/listGenre.js
@@ -3,24 +3,40 @@ define(['knockout', 'dataService', 'postman'], function (ko, ds, postman) {
         let genreObject = ko.observable();
         let titles = ko.observableArray([]);
         let titleId = ko.observable();
+        
+        let genres = ko.observableArray();
+        let selectedGenre = ko.observableArray([10]);
 
+        
+        /* Populating the page with all all titles */
         let titleDetails = (data) => {
             postman.publish("titleDetails", data);
-            /*postman.publish("changeView", "title-details");*/
+            
         }
 
-        postman.subscribe("listTitlesByGenre", data => {
-            genreObject(data);
-            console.log(data)
-            console.log(genreObject()); 
-        });
-
-        ds.getTitles(data => {
+        ds.getTitlesByGenre(data => {
             console.log(data);
                 data["data"]["$values"].forEach(title => {
                 titles.push(title);
             });
         });
+        
+        /*Trying to pupulate the page with selected genre titles*/ 
+
+        //Henriks code
+        selectedPageSize.subscribe(() => {
+            var genre = selectedGenre()[0];
+            getData(ds.searchTitleByGenre(genre));
+        });
+        
+        postman.subscribe("listTitlesByGenre", data => {
+            
+            console.log(data)
+            console.log(genreObject()); 
+        });
+        
+
+       
         
         return {
             titles,
