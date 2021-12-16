1771529f792e0fade27f2373d4eb93aeebc2fd8b
Hammi007 <h.e.4000@hotmail.com>
2021-12-16 11:48:35 +0100

    Fixed stars for title_Details + cleanuo

--- js/components/titles/titleDetails.js
+++ js/components/titles/titleDetails.js
@@ -2,40 +2,18 @@ define(['knockout', 'dataService', 'postman'], function (ko, ds, postman) {
     return function (params) {
         let titleObject = ko.observable();
 
-        let stars = ko.observableArray([]);
-       
-
-
         postman.subscribe("titleDetails", data => {
             console.log(data);
             ds.getTitle(data.titleId, setData);
-            ds.getStars(data.titleId, setData2);
         });
 
         function setData(data) 
         {
-        
-            titleObject(data);
-            
-        }
-
-        function setData2(data) 
-        {
-            console.log("before");
-            console.log(data);
-
-            data["result"]["$values"].forEach(item => {
-                stars.push(item);    
-            });
-
-            //stars(data);
-            console.log("ressult");
-            console.log(stars());
+            titleObject(data);   
         }
 
         return {
-            titleObject,
-            stars
+            titleObject
         }
     };
 });
